b315cc4d44e7f0fe428117be1a31548789ccac72
emerson arruda <emersongitgithub@gmail.com>
2022-12-29 21:48:48 -0300

    terminei o html e CSS da avaliação dos clientes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 
-const header = document.querySelector('header');
 
-window.addEventListener("scroll", () => {
-    header.classList.toggle('sticky', this.window.scrollY > 0);
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
 })
 
- 
+ let menu = document.querySelector("#menu-icon");
+ let navmenu = document.querySelector(".navmenu");
+
+ menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navmenu.classList.toggle("open");  
+ }
